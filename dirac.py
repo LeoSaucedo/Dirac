b50e22e5a753ec502afbed1995be6283289316d3
Carlos Saucedo <carlos@cgsphoto.com>
2019-01-19 22:46:05 -0500

    Added initial paypal.

--- dirac.py
+++ dirac.py
@@ -4,10 +4,12 @@ Carlos Saucedo, David Risi, Liz Parra
 2019
 """
 
-import json, os
+import json, os, dwollav2
 import Mods.wolfram as wolfram
 import Mods.CleverApi as cleverbot
 from googletrans import Translator
+
+finlit = False
 
 with open("config.json","r") as h:
     config = json.load(h)
@@ -40,11 +42,25 @@ def ask(queryText):
     response using a variety
     of APIs.
     """
-
-    # Dwolla
-
+    # FinLit module.
+    if(queryText.lower().includes("play")):
+        response = "Want to play some of my financial games?"
+        response += "\nWhich one? Tip Game or Interest game?"
+        return response
+    elif(queryText.lower().includes("tip")):
+        # Tip game
+        response = "Great! Here's the Tip Game: "
+        return response
+    elif(queryText.lower().includes("interest")):
+        # Interest game
+        response = "Great! Here's the Interest Game:"
+    
+    # PayPal.me integration.
+    elif(queryText.lower().startswith("pay")):
+        rawInput = (getRawText(queryText, "pay")).replace("$", "").split
+        return("https://paypal.me/" + rawInput[1] + "/" + rawInput[2])
     # Google Translate integration.
-    if(queryText.lower().startswith("translate")):
+    elif(queryText.lower().startswith("translate")):
         return trans.translate(getRawText(queryText, "translate")).text
 
     # OwO
